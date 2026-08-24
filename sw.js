a31bac10be033c2a7bef46c62987f6497e4f8a34
likklemore1126 <159778980+likklemore1126@users.noreply.github.com>
2026-08-24 20:39:24 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = '2026-08-22t';
+const SW_VERSION = '2026-08-22u';
 const CACHE_NAME = `yubisashi-thai-${SW_VERSION}`;
 
 const PRECACHE_URLS = [
@@ -157,6 +157,9 @@ const PRECACHE_URLS = [
   './images/manners/manner_temple.jpg',
   './images/manners/manner_royal.jpg',
   './images/manners/manner_food.jpg',
+  './images/packing/plug_a_to_c.jpg',
+  './images/packing/plug_c_thai.jpg',
+  './images/packing/outlet_thai.jpg',
 ];
 
 self.addEventListener('install', (event) => {
